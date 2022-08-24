bba120485e376a3369f3f4fed9429d7206584339
theodore cleveland <ted.cleveland@gmail.com>
2022-08-24 16:51:42 +0000

    update notes

---
+++ ce3105notes/_build/jupyter_execute/lessons/laboratory6.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Momentum Transfer
+# 
+# 
+
+# ## Water Jets
+
+# ## Conservation of Linear Momentum
+
+# ## Experimental Set-Up
+
+# ## References
+# 
+
+# In[ ]:
+
+
+
+
