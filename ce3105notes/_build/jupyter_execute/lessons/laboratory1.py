0217c348277e3e9f9c423a17dd90bddcc602dfb0
theodore cleveland <ted.cleveland@gmail.com>
2022-08-26 16:01:02 +0000

    laboratory0

---
+++ ce3105notes/_build/jupyter_execute/lessons/laboratory1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Fluid Properties
+# 
+# 
+
+# ## Density
+
+# ## Specific Gravity
+
+# ## Viscosity
+
+# ## References
+# 
+
+# 1. [Laboratory 1 ](http://54.243.252.9/ce-3105-webroot/2-Exercises/laboratory1/laboratory1.html)
+
+# In[ ]:
+
+
+
+
---
+++ ce3105notes/_build/jupyter_execute/lessons/laboratory4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Pipe Friction
+# 
+# 
+
+# ## Modified Bernoulli Equation
+
+# ## Flow Regimes
+
+# ## Friction Factors
+
+# ## References
+# 
+
+# In[ ]:
+
+
+
+
