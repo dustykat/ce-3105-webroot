3ba8a81c0596fe7c7398288048c6bf63d4ceac0a
theodore cleveland <ted.cleveland@gmail.com>
2022-10-08 22:15:23 +0000

    emergency update failover server drop

---
+++ ce3105notes/_build/jupyter_execute/lessons/laboratory4/laboratory4.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 4. Pipe Friction
+# 
+# 1 Objectives
+#  Observe the behavior of ow with change in velocity and Reynolds
+# number
+#  Observe the variation of frictional loss with change in velocity
+# 2 Theory
+# Laminar ow occurs at low velocities when the particles of waters move in
+# parallel straight lines. The ow is smooth and well-ordered. As the velocity
+# increases, the movement becomes undulating and at one point it breaks into
+# vortices. The ow is turbulent in this case. Reynolds number can determine
+# whether the ow is laminar or turbulent. For a circular pipe, it can be ex-
+# pressed as
+# Re = vD
+#  (1)
+# or
+# Re = vD
+#  (where  = 
+#  ) (2)
+# It is nearly impossible for turbulent ow to occur at Reynolds number less
+# than 2000 as the turbulence will be restrained by the viscous resistance.
+# However, there is a stage when laminar ow is becoming turbulent or turbu-
+# lent becoming laminar. During this time, the ow is transitional ow. For
+# smooth pipes, the common rule is
+#  Laminar Flow: Re < 2000
+#  Transitional Flow: Re = 2000 to 4000
+#  Turbulent Flow: Re > 4000
+# For laminar ow, the head loss due to friction is directly proportional to the
+# velocity:
+# i / v
+# 
+
+# ## Modified Bernoulli Equation
+
+# ## Flow Regimes
+
+# ## Friction Factors
+
+# ## Link to Laboratory Document
+# 
+# 1. [Laboratory 4 Tasklist](http://54.243.252.9/ce-3105-webroot/2-Exercises/laboratory4/laboratory4.html)
+# 2. [Laboratory 4 Instructional Video by Dr. Uddameri](https://www.youtube.com/watch?v=Xu132UBglpw)
+
+# ## References
+# 
+
+# In[ ]:
+
+
+
+
